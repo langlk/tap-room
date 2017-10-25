21ea25249012958b74204465dc84ae0e3707d08a
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-25 11:49:57 -0700

    Add card for display

--- app/keg-details.component.ts
+++ app/keg-details.component.ts
@@ -4,25 +4,30 @@ import { Keg } from './keg.model';
 @Component({
   selector: 'keg-details',
   template: `
-    <div *ngIf=focusKeg>
-      <h2>
-        {{focusKeg.name}}
-        <a (click)="edit()" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">mode_edit</i></a>
-      </h2>
-      <h3>{{focusKeg.brand}}</h3>
-      <p>
-        <strong>Price/Pint:</strong>
-        {{"$" + focusKeg.price}}
-      <p>
-      <p>
-        <strong>ABV:</strong>
-        {{focusKeg.abv}}%
-      </p>
-      <p>
-        <strong>Pints Remaining:</strong>
-        {{focusKeg.volume}}
-      </p>
+    <div *ngIf=focusKeg class="card">
+      <div class="card-image">
+        <img src="http://nordictravelmag.com/wp-content/uploads/2017/06/01.jpg">
+        <h2 class="card-title">{{focusKeg.name}}</h2>
+        <a (click)="edit()" class="btn-floating waves-effect halfway-fab waves-light red right"><i class="material-icons">mode_edit</i></a>
+      </div>
+      <div class="card-content">
+        <h3 class="card-title">{{focusKeg.brand}}</h3>
+        <p>
+          <strong>Price/Pint:</strong>
+          {{"$" + focusKeg.price}}
+        <p>
+        <p>
+          <strong>ABV:</strong>
+          {{focusKeg.abv}}%
+        </p>
+        <p>
+          <strong>Pints Remaining:</strong>
+          {{focusKeg.volume}}
+        </p>
+
+      </div>
     </div>
+
   `
 })
 
