28dcd31a16b6a97c9cc8915d235379da73e3e11f
Kelsey Langlois <kels.langlois@gmail.com>
2017-10-26 11:16:45 -0700

    Update happy hour to calculate using inputs.

--- app/happy-hour.model.ts
+++ app/happy-hour.model.ts
@@ -2,6 +2,10 @@ export class HappyHour {
   constructor(public startTime: number, public endTime: number, public discount: number) { }
 
   isHappyHour(currentTime) {
-    return (currentTime.getHours() >= 16 && currentTime.getHours() < 18);
+    return (currentTime.getHours() >= this.startTime && currentTime.getHours() < this.endTime);
+  }
+
+  startFormatted() {
+
   }
 }
